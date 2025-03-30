9dffd8bd2e9e52d08c74d034785aaa830ec2892d
altamsh <bairagdaraltamsh@gmail.com>
2025-03-31 01:42:01 +0530

    Add: Vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import MemeMaker from './pages/MemeMaker';
+import { Analytics } from "@vercel/analytics/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import MemeMaker from "./pages/MemeMaker";
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/meme-maker" element={<MemeMaker />} />
-        {/* Other routes */}
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
